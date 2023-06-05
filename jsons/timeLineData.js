222b7b2129dbf73d00257e4a60284504f79cd113
Duynt <duynt@xdp.vn>
2023-06-05 12:06:15 +0700

    Public json v1

---
+++ jsons/timeLineData.js
@@ -0,0 +1,414 @@
+export default [
+    {
+        insite: true,
+        day: 7,
+        addDay: 14,
+        name_t: "time_line.week2",
+        title_t: "time_line.week2_title",
+        description_t: "time_line.week2_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 21,
+        name_t: "time_line.week3",
+        title_t: "time_line.week3_title",
+        description_t: "time_line.week3_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 28,
+        name_t: "time_line.week4",
+        title_t: "time_line.week4_title",
+        description_t: "time_line.week4_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 35,
+        name_t: "time_line.week5",
+        title_t: "time_line.week5_title",
+        description_t: "time_line.week5_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 42,
+        name_t: "time_line.week6",
+        title_t: "time_line.week6_title",
+        description_t: "time_line.week6_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 49,
+        name_t: "time_line.week7",
+        title_t: "time_line.week7_title",
+        description_t: "time_line.week7_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 56,
+        name_t: "time_line.week8",
+        title_t: "time_line.week8_title",
+        description_t: "time_line.week8_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 63,
+        name_t: "time_line.week9",
+        title_t: "time_line.week9_title",
+        description_t: "time_line.week9_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 70,
+        name_t: "time_line.week10",
+        title_t: "time_line.week10_title",
+        description_t: "time_line.week10_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 77,
+        name_t: "time_line.week11",
+        title_t: "time_line.week11_title",
+        description_t: "time_line.week11_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 84,
+        name_t: "time_line.week12",
+        title_t: "time_line.week12_title",
+        description_t: "time_line.week12_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 91,
+        name_t: "time_line.week13",
+        title_t: "time_line.week13_title",
+        description_t: "time_line.week13_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 98,
+        name_t: "time_line.week14",
+        title_t: "time_line.week14_title",
+        description_t: "time_line.week14_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 105,
+        name_t: "time_line.week15",
+        title_t: "time_line.week15_title",
+        description_t: "time_line.week15_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 112,
+        name_t: "time_line.week16",
+        title_t: "time_line.week16_title",
+        description_t: "time_line.week16_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 119,
+        name_t: "time_line.week17",
+        title_t: "time_line.week17_title",
+        description_t: "time_line.week17_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 126,
+        name_t: "time_line.week18",
+        title_t: "time_line.week18_title",
+        description_t: "time_line.week18_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 133,
+        name_t: "time_line.week19",
+        title_t: "time_line.week19_title",
+        description_t: "time_line.week19_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 140,
+        name_t: "time_line.week20",
+        title_t: "time_line.week20_title",
+        description_t: "time_line.week20_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 147,
+        name_t: "time_line.week21",
+        title_t: "time_line.week21_title",
+        description_t: "time_line.week21_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 154,
+        name_t: "time_line.week22",
+        title_t: "time_line.week22_title",
+        description_t: "time_line.week22_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 161,
+        name_t: "time_line.week23",
+        title_t: "time_line.week23_title",
+        description_t: "time_line.week23_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 168,
+        name_t: "time_line.week24",
+        title_t: "time_line.week24_title",
+        description_t: "time_line.week24_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 176,
+        name_t: "time_line.week25",
+        title_t: "time_line.week25_title",
+        description_t: "time_line.week25_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 183,
+        name_t: "time_line.week26",
+        title_t: "time_line.week26_title",
+        description_t: "time_line.week26_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 190,
+        name_t: "time_line.week27",
+        title_t: "time_line.week27_title",
+        description_t: "time_line.week27_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 197,
+        name_t: "time_line.week28",
+        title_t: "time_line.week28_title",
+        description_t: "time_line.week28_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 204,
+        name_t: "time_line.week29",
+        title_t: "time_line.week29_title",
+        description_t: "time_line.week29_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 211,
+        name_t: "time_line.week30",
+        title_t: "time_line.week30_title",
+        description_t: "time_line.week30_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 218,
+        name_t: "time_line.week31",
+        title_t: "time_line.week31_title",
+        description_t: "time_line.week31_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 225,
+        name_t: "time_line.week32",
+        title_t: "time_line.week32_title",
+        description_t: "time_line.week32_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 232,
+        name_t: "time_line.week33",
+        title_t: "time_line.week33_title",
+        description_t: "time_line.week33_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 239,
+        name_t: "time_line.week34",
+        title_t: "time_line.week34_title",
+        description_t: "time_line.week34_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 246,
+        name_t: "time_line.week35",
+        title_t: "time_line.week35_title",
+        description_t: "time_line.week35_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 253,
+        name_t: "time_line.week36",
+        title_t: "time_line.week36_title",
+        description_t: "time_line.week36_description",
+        require: true
+    },
+    {
+        insite: true,
+        day: 7,
+        addDay: 260,
+        name_t: "time_line.week37",
+        title_t: "time_line.week37_title",
+        description_t: "time_line.week37_description",
+        require: true
+    },
+    // {
+    //     insite: true,
+    //     day: 35,
+    //     addDay: 0,
+    //     name_t: "time_line.week1_5",
+    //     title_t: "time_line.week1_5_title",
+    //     description_t: "time_line.week1_5_description",
+
+    // },
+    // {
+    //     insite: true,
+    //     day: 14,
+    //     addDay: 49,
+    //     name_t: "time_line.week7_8",
+    //     title_t: "time_line.week7_8_title",
+    //     description_t: "time_line.week7_8_description",
+
+    // },
+    // {
+    //     insite: true,
+    //     day: 21,
+    //     addDay: 77,
+    //     name_t: "time_line.week11_13",
+    //     title_t: "time_line.week11_13_title",
+    //     description_t: "time_line.week11_13_description",
+
+    // 
+    // },
+    // {
+    //     insite: true,
+    //     day: 21,
+    //     addDay: 112,
+    //     name_t: "time_line.week16_18",
+    //     title_t: "time_line.week16_18_title",
+    //     description_t: "time_line.week16_18_description",
+
+    // 
+    // },
+    // {
+    //     insite: true,
+    //     day: 21,
+    //     addDay: 154,
+    //     name_t: "time_line.week22_24",
+    //     title_t: "time_line.week22_24_title",
+    //     description_t: "time_line.week22_24_description",
+
+    // 
+    // },
+    // {
+    //     insite: true,
+    //     day: 28,
+    //     addDay: 182,
+    //     name_t: "time_line.week26_30",
+    //     title_t: "time_line.week26_30_title",
+    //     description_t: "time_line.week26_30_description",
+
+    // 
+    // },
+    // {
+    //     insite: true,
+    //     day: 7,
+    //     addDay: 224,
+    //     name_t: "time_line.week32",
+    //     title_t: "time_line.week32_title",
+    //     description_t: "time_line.week32_description",
+
+    // 
+    // },
+    // {
+    //     insite: true,
+    //     day: 14,
+    //     addDay: 245,
+    //     name_t: "time_line.week35_36",
+    //     title_t: "time_line.week35_36_title",
+    //     description_t: "time_line.week35_36_description",
+
+    // 
+    // },
+    // {
+    //     insite: true,
+    //     day: 7,
+    //     addDay: 259,
+    //     name_t: "time_line.week37",
+    //     title_t: "time_line.week37_title",
+    //     description_t: "time_line.week37_description",
+
+    // 
+    // },
+]
